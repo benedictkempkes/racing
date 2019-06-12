ece697a2c044cf6669c59f2a0f1006c8195c8c01
Benedict Kempkes <benedictkempkes@googlemail.com>
2019-06-12 23:13:13 +0200

    Calendar

--- src/components/views/calendar/Calendar.js
+++ src/components/views/calendar/Calendar.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import './calendar.less';
 
 import Event from './Event';
 
+const getCalendar = (calendar) => {
+    return calendar.map((item, index) => (
+        <div className="wrapper" key={index}>
+            {(index%2) ? 
+                <div className="row">
+                    <div className="itemWrapper" style={{ justifyContent: 'flex-end', marginLeft: '10px'}}>
+                        <Event key={index} place={item['1']} time={item['2']} position={item['3']} race={item['4']} image={item['5']} styling='left'/>
+                        <span className="horizontal"></span>
+                        <span className="vertical"></span>
+                        <span className="circle" style={{ right: '-10px' }}></span>
+                    </div>
+                    <div className="otherSide"></div>
+                </div>
+                 : 
+                <div className="row">
+                    <div className="otherSide"></div>
+                    <div className="itemWrapper" style={{ justifyContent: 'flex-start', marginRight: '10px'}}>
+                        <span className="circle" style={{left: '-10px'}}></span>
+                        <span className="vertical"></span>
+                        <span className="horizontal"></span>
+                        <Event key={index} place={item['1']} time={item['2']} position={item['3']} race={item['4']} image={item['5']} styling='right' />
+                    </div>
+                </div>
+                 }
+        </div>
+    ))
+}
+
 const Calendar = ({ calendar }) => (
-    <div>
-        {
-            calendar.map((item, index) => (
-                <Event key={index} place={item['1']} time={item['2']} position={item['3']} race={item['4']} />
-            ))
-        }
+    <div className='calendar'>
+        <div className="calendarWrapper">
+            {getCalendar(calendar)}
+        </div>
     </div>
 )
 
--- src/components/views/calendar/Event.js
+++ src/components/views/calendar/Event.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import './event.less';
 
 import EventController from './EventController';
 import { ERGEBNIS } from '../../../constants';
 
-const Event = ({ place, position, time, race, serie, oldRace  }) => (
-    <div>
-        <h2>{place}</h2>
-        <h3>{time}</h3>
-        <h3>{(position) ? 'Platz ' + position : <span>&nbsp;</span>}</h3>
-        <div>
-            <EventController page={ERGEBNIS} serie={serie} style='calendarButton' race={race} oldRace={oldRace}>
-                Ergebnis
-            </EventController>
+const Event = ({ place, position, time, race, serie, oldRace, image, styling  }) => (
+    <div className={"event " + styling}>
+        <div className="image" style={{backgroundImage: 'url(' + image + ')'}}>
+
+        </div>
+        <div className="content">
+            <div className="header">
+                <h2>{place}</h2>
+                <h3>{time}</h3>
+                <h3>{(position) ? 'Platz ' + position : <span>&nbsp;</span>}</h3>
+            </div>
+            <div className="controls">
+                {console.log(race)}
+                <EventController page={ERGEBNIS} serie={serie} style='calendarButton' race={race} oldRace={oldRace}>
+                    Ergebnis
+                </EventController>
+            </div>
         </div>
     </div>
 )
