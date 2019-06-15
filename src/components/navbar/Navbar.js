572a03ad0df3921e2880ceb22390faa7a6aed8a9
Benedict Kempkes <benedictkempkes@googlemail.com>
2019-06-15 12:31:44 +0200

    Mobile bug

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,32 +6,55 @@ import { toggleMenu } from '../../actions';
 
 import NavbarController from './NavbarController';
 
-const Navbar = ({ pagination, serie, onClick, menu }) => (
-    <div className="navbar">
-        <div className="navbarItem back">
-            {(pagination.BACK) ? <NavbarController page={pagination.BACK} style='navbarButton'>{pagination.BACK.NAME}</NavbarController> : undefined}
-        </div>
-        <div className="navbarItem current" style={(pagination.NAME === 'Gosila') ? {justifyContent: 'center', margin: '0px'}: {}}>
-            {(pagination.NAME === 'Gosila') ? pagination.NAME : serie['1']}
-        </div>
-        <div className="navbarItem next">
-            {(pagination.NEXT) ? <NavbarController page={pagination.NEXT} style='navbarButton'>{pagination.NEXT.NAME}</NavbarController> : undefined}
-        </div>
-        {(pagination.BACK || pagination.NEXT) ? <button className="menuButtons" onClick={() => onClick((menu) ? false : true)}>
-            <span className="circle"></span>
-            <span className="circle"></span>
-            <span className="circle"></span>
-        </button> : undefined}
-        {(menu) ? <div className="menu" onBlur={() => onClick(false)}>
-            <div className="navbarItem">
-                {(pagination.BACK) ? <NavbarController page={pagination.BACK} style='navbarButton'>{pagination.BACK.NAME}</NavbarController> : undefined}
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    componentDidMount() {
+        document.addEventListener('click', this.handleClick);
+    }
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClick);
+    }
+    handleClick(e){
+        console.log(e.target.className );
+        if (e.target.className !== 'navbarButton' &&
+            e.target.className !=='menuButtons' &&
+            e.target.className !== 'circle'){
+            this.props.onClick(false);
+        }
+    }
+    render() {
+        const { pagination, serie, onClick, menu } = this.props;
+        return (
+            <div className="navbar">
+                <div className="navbarItem back">
+                    {(pagination.BACK) ? <NavbarController page={pagination.BACK} style='navbarButton'>{pagination.BACK.NAME}</NavbarController> : undefined}
+                </div>
+                <div className="navbarItem current" style={(pagination.NAME === 'Gosila') ? { justifyContent: 'center', margin: '0px' } : {}}>
+                    {(pagination.NAME === 'Gosila') ? pagination.NAME : serie['1']}
+                </div>
+                <div className="navbarItem next">
+                    {(pagination.NEXT) ? <NavbarController page={pagination.NEXT} style='navbarButton'>{pagination.NEXT.NAME}</NavbarController> : undefined}
+                </div>
+                {(pagination.BACK || pagination.NEXT) ? <button className="menuButtons" onClick={() => onClick((menu) ? false : true)}>
+                    <span className="circle"></span>
+                    <span className="circle"></span>
+                    <span className="circle"></span>
+                </button> : undefined}
+                {(menu) ? <div className="menu" onBlur={() => onClick(false)}>
+                    <div className="navbarItem">
+                        {(pagination.BACK) ? <NavbarController page={pagination.BACK} style='navbarButton'>{pagination.BACK.NAME}</NavbarController> : undefined}
+                    </div>
+                    <div className="navbarItem">
+                        {(pagination.NEXT) ? <NavbarController page={pagination.NEXT} style='navbarButton'>{pagination.NEXT.NAME}</NavbarController> : undefined}
+                    </div>
+                </div> : undefined}
             </div>
-            <div className="navbarItem">
-                {(pagination.NEXT) ? <NavbarController page={pagination.NEXT} style='navbarButton'>{pagination.NEXT.NAME}</NavbarController> : undefined}
-            </div>
-        </div> : undefined}
-    </div>
-)
+        );
+    }
+}
 
 const mapStateToProps = state => ({
     pagination: state.pagination,
@@ -39,7 +62,7 @@ const mapStateToProps = state => ({
     menu: state.menu
 })
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
+const mapDispatchToProps = (dispatch) => ({
     onClick: value => dispatch(toggleMenu(value))
 })
 
